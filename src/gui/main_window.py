f88d64ce7eaf2eb5728ae288f41b0748c8fcfd77
Joel Wallace <joel.wallace161@gmail.com>
2025-05-30 17:13:06 +0100

    init main_window.py, added scaffolding widgets

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -9,10 +9,12 @@ import pyqtgraph as pg
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("NMR-Proc")
+        self.setWindowTitle("NMR-Proc 0.2.0")
         self.resize(1000, 600)
-
+        self.init_main_ui()
         # State should go somewhere in here
+    
+    def init_main_ui(self):
         main_widget = QWidget()
         main_layout = QVBoxLayout()
         self.setCentralWidget(main_widget)
@@ -20,7 +22,12 @@ class MainWindow(QMainWindow):
         left_main_panel = QSplitter(Qt.Vertical)
         self.plot_widget = pg.PlotWidget()
 
+        # Create file_tree and add to left_main_panel
+        # Create tab panel and add to left_main_panel
+        left_main_panel.setSizes([40,60])
+
         main_splitter.addWidget(left_main_panel)
         main_splitter.addWidget(self.plot_widget)
+        main_splitter.setSizes([30,70])
         main_layout.addWidget(main_splitter)
         main_widget.setLayout(main_layout)
