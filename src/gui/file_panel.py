5eff4cb76aca8b54867215fb318492f578c4656d
Joel Wallace <joel.wallace161@gmail.com>
2025-05-30 20:14:43 +0100

    wrote FilePanel class

--- src/gui/file_panel.py
+++ src/gui/file_panel.py
@@ -1,11 +1,14 @@
 from PySide6.QtWidgets import (
     QWidget, QTreeView, QFileSystemModel,
-    QDir
+    QVBoxLayout, QPushButton, QLineEdit, 
+    QFileDialog
         )
+
+from PySide6.QtCore import QDir
 
 class FilePanel(QWidget):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         # Open directory button connected to select_directory function
         # QLineEdit with current directory, non-editable but copy-pastable
         self.tree = QTreeView()
@@ -17,4 +20,24 @@ class FilePanel(QWidget):
         self.tree.hideColumn(2)
         self.tree.hideColumn(3)
 
-        ## Need getters and setters for dir path etc
+        self.open_button = QPushButton("Select Directory")
+        self.open_button.clicked.connect(self.select_directory)
+
+        self.path_edit = QLineEdit("No directory selected")
+        self.path_edit.setReadOnly(True)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.open_button)
+        self.layout.addWidget(self.path_edit)
+        self.layout.addWidget(self.tree)
+        self.setLayout(self.layout)
+
+    def get_dir_path(self):
+        return self.path_edit.text
+    
+    def select_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if directory:
+            self.model.setRootPath(directory)
+            self.tree.setRootIndex(self.model.index(directory))
+            self.path_edit.setText(directory)
--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -6,6 +6,8 @@ from PySide6.QtCore import Qt, QDir
 
 import pyqtgraph as pg
 
+from .file_panel import FilePanel
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -23,11 +25,13 @@ class MainWindow(QMainWindow):
         self.plot_widget = pg.PlotWidget()
 
         # Create file_tree and add to left_main_panel
+        self.file_panel = FilePanel()
+        left_main_panel.addWidget(self.file_panel)
         # Create tab panel and add to left_main_panel
         left_main_panel.setSizes([40,60])
 
         main_splitter.addWidget(left_main_panel)
         main_splitter.addWidget(self.plot_widget)
-        main_splitter.setSizes([30,70])
+        main_splitter.setSizes([3000,7000])
         main_layout.addWidget(main_splitter)
         main_widget.setLayout(main_layout)
